00f4d4101fd5855d923b10ff5713177cfd8c626a
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2021-11-17 19:26:21 +0900

    test: テストにdescribeでツリー構造を追加

--- 12.inheritance/booking.test.js
+++ 12.inheritance/booking.test.js
@@ -9,57 +9,66 @@ function createPremiumBooking(show, date, extras) {
   return result;
 };
 
-test('Booking#basePrice', () => {
-  const show = {
-    price: 1000,
-    talkback: true
-  };
-  const aBooking = createBooking(show, '2021/11/17');
-  aBooking.isPeakDay = true;
-  expect(aBooking.basePrice).toBe(1150);
-});
 
-test('Booking#hasTalkback(true)', () => {
-  const show = {
-    price: 1000,
-    talkback: true
-  };
-  const aBooking = createBooking(show, '2021/11/17');
-  expect(aBooking.hasTalkback).toBe(true);
+describe('Booking#basePrice', () => {
+  test('ピーク時の料金を算出', () => {
+    const show = {
+      price: 1000,
+      talkback: true
+    };
+    const aBooking = createBooking(show, '2021/11/17');
+    aBooking.isPeakDay = true;
+    expect(aBooking.basePrice).toBe(1150);
+  });
 })
 
-test('Booking#hasTalkback(false)', () => {
-  const show = {
-    price: 1000,
-    talkback: true
-  };
-  const aBooking = createBooking(show, '2021/11/17');
-  aBooking.isPeakDay = true;
-  expect(aBooking.hasTalkback).toBe(false);
+describe('Booking#hasTalkback', () => {
+  test('ピークでなく、showにセットされているときtrueを返す', () => {
+    const show = {
+      price: 1000,
+      talkback: true
+    };
+    const aBooking = createBooking(show, '2021/11/17');
+    expect(aBooking.hasTalkback).toBe(true);
+  })
+  test('ピーク時はfalseを返す', () => {
+    const show = {
+      price: 1000,
+      talkback: true
+    };
+    const aBooking = createBooking(show, '2021/11/17');
+    aBooking.isPeakDay = true;
+    expect(aBooking.hasTalkback).toBe(false);
+  })
 })
 
-test('PremiumBooking#hasTalkback(true)', () => {
-  const show = {
-    price: 1000,
-    talkback: true
-  };
-  const extras = {
-    premiumFee: 10000,
-  };
-  const aBooking = createPremiumBooking(show, '2021/11/18', extras);
-  aBooking.isPeakDay = true;
-  expect(aBooking.hasTalkback).toBe(true);
+describe('PremiumBooking#hasTalkback(true)', () => {
+  test('ピークであってもtrueを返す', () => {
+    const show = {
+      price: 1000,
+      talkback: true
+    };
+    const extras = {
+      premiumFee: 10000,
+    };
+    const aBooking = createPremiumBooking(show, '2021/11/18', extras);
+    aBooking.isPeakDay = true;
+    expect(aBooking.hasTalkback).toBe(true);
+  })
 })
 
-test('PremiumBooking#basePrice', () => {
-  const show = {
-    price: 1000,
-    talkback: true
-  };
-  const extras = {
-    premiumFee: 10000,
-  };
-  const aBooking = createPremiumBooking(show, '2021/11/18', extras);
-  aBooking.isPeakDay = true;
-  expect(aBooking.basePrice).toBe(11150);
+describe('PremiumBooking#basePrice', () => {
+  test('extraの料金も含めた金額を算出', () => {
+    const show = {
+      price: 1000,
+      talkback: true
+    };
+    const extras = {
+      premiumFee: 10000,
+    };
+    const aBooking = createPremiumBooking(show, '2021/11/18', extras);
+    aBooking.isPeakDay = true;
+    expect(aBooking.basePrice).toBe(11150);
+  })
 })
+
