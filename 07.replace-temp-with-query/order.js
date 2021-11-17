c4b7d1862cc860195afbbfea4046fd44d4eea4ec
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2021-11-17 19:00:31 +0900

    問い合わせ(メソッド)による一時変数の置き換えでリファクタ
    名前の通り一時変数をメソッドとして切り出して名前をつける

--- 07.replace-temp-with-query/order.js
+++ 07.replace-temp-with-query/order.js
@@ -5,9 +5,16 @@ module.exports = class Order {
   }
 
   get price() {
-    var basePrice = this._quantity * this._item.price;
-    var discountFactor = 0.98;
-    if (basePrice > 1000) discountFactor -= 0.03;
-    return basePrice * discountFactor;
+    return this.basePrice * this.discountFactor;
+  }
+
+  get basePrice() {
+    return this._quantity * this._item.price;
+  }
+
+  get discountFactor() {
+    let discountFactor = 0.98;
+    if (this.basePrice > 1000) discountFactor -= 0.03;
+    return discountFactor;
   }
 }
